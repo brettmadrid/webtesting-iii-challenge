639302a7e2a3a7534facf8644583a41eeeb130a6
brettmadrid <brettmadrid9@gmail.com>
2019-04-18 18:36:39 -0700

    MVP COmplete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,37 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import renderer from "react-test-renderer";
+import "jest-dom/extend-expect";
+import Controls from "./Controls";
+
+describe("Controls Module", () => {
+  xit("should render on load", () => {
+    render(<Controls />);
+  });
+
+  xit("should display lock gate text on the button if locked is false and closed is true", () => {
+    const locked = false;
+    const closed = true;
+
+    const { getByText } = render(<Controls locked={locked} closed={closed} />);
+    const text = getByText(/lock gate/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  xit("should display open gate text on the button if locked is false and closed is true", () => {
+    const locked = false;
+    const closed = true;
+
+    const { getByText } = render(<Controls locked={locked} closed={closed} />);
+    const text = getByText(/open gate/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  it('should match the snapshot', () => {
+    const tree = renderer.create(<Controls />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  })
+  
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,65 @@
-// Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+
+const testData = {
+  closed: true,
+  locked: false
+}
+
+describe('Display Module', () => {
+  xit('should render on load', () => {
+    render(<Display />);
+  })
+
+  xit('should receive closed prop', () => {
+    const { closed } = testData;
+
+    const { getByText } = render(<Display closed={closed} />);
+    const text = getByText(/closed/i);
+    expect(text).toBeInTheDocument();
+  })
+
+  xit('should receive locked prop', () => {
+    const { locked } = testData;
+
+    const { getByText } = render(<Display locked={locked} />);
+    const text = getByText(/locked/i);
+    expect(text).toBeInTheDocument();
+  })
+
+  xit('should return gate open when locked is false and closed if false', () => {
+    const test = {
+      closed: false,
+      locked: false
+    }
+
+    const { getByText } = render((<Display isOpened={test} />));
+    const text = getByText(/open/i);
+    expect(text).toBeInTheDocument();
+  })
+
+  it('should return locked with both closed and locked are true', () => {
+    const test = {
+      closed: true,
+      locked: true
+    }
+
+    const { getByText } = render((<Display isLocked={test} />));
+    const text = getByText(/locked/i);
+    expect(text).toBeInTheDocument();
+  })
+
+  xit('should return unlocked with both closed and locked are true', () => {
+    const test = {
+      closed: true,
+      locked: false
+    }
+
+    const { getByText } = render((<Display isUnlocked={test} />));
+    const text = getByText(/unlocked/i);
+    expect(text).toBeInTheDocument();
+  })
+})
+
